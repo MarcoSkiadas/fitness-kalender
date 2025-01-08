a1bc11d3193f46f6b0d2264df154a13e7752c266
MarcoSkiadas <Skiadas2@hotmail.com>
2025-01-08 17:55:11 +0100

    Adding Hashcode/ToString/Equals to Set for reliability

--- backend/src/main/java/org/example/backend/model/Set.java
+++ backend/src/main/java/org/example/backend/model/Set.java
@@ -3,6 +3,8 @@ package org.example.backend.model;
 import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Objects;
 
 public record Set(
         @Id
@@ -15,5 +17,30 @@ public record Set(
 ) {
         public String getSetId() {
                 return id;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                Set set = (Set) o;
+                return Objects.equals(id, set.id) && Objects.equals(name, set.name) && Objects.equals(userId, set.userId) && Objects.deepEquals(exercise, set.exercise) && Objects.equals(createdAt, set.createdAt) && Objects.equals(updatedAt, set.updatedAt);
+        }
+
+        @Override
+        public int hashCode() {
+                return Objects.hash(id, userId, name, Arrays.hashCode(exercise), createdAt, updatedAt);
+        }
+
+        @Override
+        public String toString() {
+                return "Set{" +
+                        "id='" + id + '\'' +
+                        ", userId='" + userId + '\'' +
+                        ", name='" + name + '\'' +
+                        ", exercise=" + Arrays.toString(exercise) +
+                        ", createdAt=" + createdAt +
+                        ", updatedAt=" + updatedAt +
+                        '}';
         }
 }
